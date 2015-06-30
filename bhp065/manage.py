68995010da90bfb69073b92a2012e2cdc5e0bd48
Erik van Widenfelt <ew2789@gmail.com>
2015-06-30 15:53:36 +0200

    Update manage.py

--- bhp065/manage.py
+++ bhp065/manage.py
@@ -3,9 +3,9 @@ import os
 import sys
 from unipath import Path
 
-SOURCE_ROOT = Path(os.path.dirname(os.path.realpath(__file__))).ancestor(2)  # e.g. /home/django/source
-EDC_DIR = SOURCE_ROOT.child('edc_project')  # e.g. /home/django/source/edc_project/
-LIS_DIR = SOURCE_ROOT.child('lis_project')  # e.g. /home/django/source/lis_project/
+SOURCE_ROOT = Path(os.path.dirname(os.path.realpath(__file__))).ancestor(2)
+EDC_DIR = SOURCE_ROOT.child('edc_project')
+LIS_DIR = SOURCE_ROOT.child('lis_project')
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
@@ -13,4 +13,3 @@ if __name__ == "__main__":
     sys.path.insert(1, LIS_DIR)
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
-
