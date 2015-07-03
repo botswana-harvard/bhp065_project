53c5f51d7bfe8f9cb05d9929d038d0f6c43735f7
melissa kewagamang <mrbkew@gmail.com>
2015-07-03 16:19:26 +0200

    choices

--- bhp065/apps/hnscc_subject/models/__init__.py
+++ bhp065/apps/hnscc_subject/models/__init__.py
@@ -1,1 +1,2 @@
 from .hnscc_off_study import HnsccOffStudy
+from .choices import HIV_STATUS, SMOKING_STATUS, VISIT_REASON, OFF_STUDY_REASON
---
+++ bhp065/apps/hnscc_subject/models/choices.py
@@ -0,0 +1,16 @@
+HIV_STATUS = (
+    ('INFECTED', 'HIV infected'),
+    ('UNINFECTED', 'HIV uninfected'),)
+
+SMOKING_STATUS = (
+    ('smoker', 'smoker'),
+    ('non-smoker', 'non-smoker'),
+    ('UNK', 'Unknown'), )
+
+VISIT_REASON = (
+    ('UNSCHEDULED', 'Unscheduled visit'),
+    ('off study', 'Off study'), )
+
+OFF_STUDY_REASON = (
+    ('Completion of protocol', 'Completion of protocol'),
+    ('Other, specify: ', 'Other, specify: '), )
