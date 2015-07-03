5ddfe2acac2ebe2496ee7c70bca845902e48ccc3
melissa kewagamang <mrbkew@gmail.com>
2015-07-03 16:35:25 +0200

    __init__ update

--- bhp065/apps/hnscc_subject/models/__init__.py
+++ bhp065/apps/hnscc_subject/models/__init__.py
@@ -1,3 +1,4 @@
 from .hnscc_off_study import HnsccOffStudy
 from .choices import HIV_STATUS, SMOKING_STATUS, VISIT_REASON, OFF_STUDY_REASON
 from .hnscc_visit import HnsccVisit
+from .enrollment import Enrollment
