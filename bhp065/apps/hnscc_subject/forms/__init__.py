b0a38e2c1dccccf8aac1ca332682982f2b7193d7
melissa kewagamang <mrbkew@gmail.com>
2015-07-06 12:36:34 +0200

    forms

--- bhp065/apps/hnscc_subject/forms/__init__.py
+++ bhp065/apps/hnscc_subject/forms/__init__.py
@@ -1,4 +1,4 @@
-from .main import HnsccVisitVisitForm, HnsccOffStudyOffStudyForm
+from .main import HnsccVisitForm, HnsccOffStudyForm
 from .enrollment_form import EnrollmentForm
 from .contemporary_form import ContemporaryForm
 from .historical_form import HistoricalForm
