31a0d4f44e46d62aad508cb9d57bb5871ea16403
melissa kewagamang <mrbkew@gmail.com>
2015-07-07 15:48:54 +0200

    enrollment admin

--- bhp065/apps/hnscc_subject/admin/enrollment_admin.py
+++ bhp065/apps/hnscc_subject/admin/enrollment_admin.py
@@ -14,13 +14,12 @@ class EnrollmentAdmin(BaseModelAdmin):
     def __init__(self, *args, **kwargs):
         super(EnrollmentAdmin, self).__init__(*args, **kwargs)
         self.fields = [
-            'registered_subject',
-            'subject_identifier',
             'report_datetime',
             'gender',
             'age',
             'hiv_status',
             'smoking_status', ]
-        self.radio_fields = {'hiv_status': admin.VERTICAL,
+        self.radio_fields = {'gender': admin.VERTICAL,
+                             'hiv_status': admin.VERTICAL,
                              'smoking_status': admin.VERTICAL, }
 admin.site.register(Enrollment, EnrollmentAdmin)
