4269c8ff6484be25790dd56a60f58f4b0aad8d75
melissa kewagamang <mrbkew@gmail.com>
2015-07-09 19:44:07 +0200

    localapps

--- bhp065/config/installed_apps/local_apps.py
+++ bhp065/config/installed_apps/local_apps.py
@@ -1,7 +1,7 @@
 LOCAL_APPS = (
-    # 'apps.hnscc',
-    # 'apps.hsncc.app_configuration',
-    'lis',
-    'edc',
+    'bhp065.apps.hnscc',
+    'bhp065.apps.hnscc.app_configuration',
+    'bhp065.apps.hnscc_dashboard',
+    'bhp065.apps.hnscc_lab',
     'bhp065.apps.hnscc_subject',
 )
