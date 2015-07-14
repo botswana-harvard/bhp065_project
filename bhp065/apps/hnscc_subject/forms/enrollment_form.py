4d4b507071133e055270415c240cf06b1da5e8d7
melissa kewagamang <mrbkew@gmail.com>
2015-07-14 11:11:40 +0200

    enrollment validation

--- bhp065/apps/hnscc_subject/forms/enrollment_form.py
+++ bhp065/apps/hnscc_subject/forms/enrollment_form.py
@@ -1,8 +1,19 @@
+from django import forms
 from edc.base.form.forms import BaseModelForm
 from ..models import Enrollment
 
 
 class EnrollmentForm (BaseModelForm):
+
+    def clean(self):
+
+        cleaned_data = self.cleaned_data
+        if cleaned_data.get('age') < 18:
+            raise forms.ValidationError('Subject is TOO YOUNG and CANNOT be enrolled')
+        if cleaned_data.get('age') > 130:
+            raise forms.ValidationError('Subject is TOO OLD and CANNOT be enrolled')
+        cleaned_data = super(EnrollmentForm, self).clean()
+        return cleaned_data
 
     class Meta:
         model = Enrollment
