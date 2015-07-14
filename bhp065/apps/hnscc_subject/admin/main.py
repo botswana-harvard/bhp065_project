809c33667b5a61e762d21e0e97c9c677a12e0853
melissa kewagamang <mrbkew@gmail.com>
2015-07-14 11:10:55 +0200

    update admin visit

--- bhp065/apps/hnscc_subject/admin/main.py
+++ bhp065/apps/hnscc_subject/admin/main.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+# from edc.base.modeladmin.admin import BaseModelAdmin
 from edc.subject.appointment.admin import BaseAppointmentModelAdmin
 
 from ..models import HnsccVisit, HnsccOffStudy
+from ...hnscc_lab.models import HnsccRequisition
 from ..forms import HnsccVisitForm, HnsccOffStudyForm
 from .hnscc_off_study_model_admin import HnsccOffStudyModelAdmin
 
@@ -11,26 +13,28 @@ class HnsccVisitAdmin(BaseAppointmentModelAdmin):
 
     form = HnsccVisitForm
     visit_model_instance_field = 'hnscc_visit'
-#     requisition_model = AnonymousRequisition
+    requisition_model = HnsccRequisition
     dashboard_type = 'subject'
     date_heirarchy = 'report_datetime'
     list_display = (
         'appointment',
         'report_datetime',
-        'reason',
+        'data_type',
         'created',
         'user_created',)
     list_filter = (
         'report_datetime',
-        'reason',)
+        'reason',
+        'data_type',)
     search_fields = (
         'appointment__registered_subject__subject_identifier',)
     fields = (
         "appointment",
         "report_datetime",
-        "reason",)
+        "reason",
+        "data_type",)
     radio_fields = {
-        "reason": admin.VERTICAL}
+        "data_type": admin.VERTICAL}
 admin.site.register(HnsccVisit, HnsccVisitAdmin)
 
 
@@ -40,7 +44,8 @@ class HnsccOffStudyAdmin(HnsccOffStudyModelAdmin):
         "registered_subject",
         "hnscc_visit",
         "offstudy_date",
-        "reason",)
+        "reason",
+        "reason_other")
 
     def formfield_for_foreignkey(self, db_field, request, **kwargs):
         if db_field.name == "hnscc_visit":
