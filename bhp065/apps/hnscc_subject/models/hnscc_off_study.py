4e9c34d2167e3b616eb7b283c721dee79d07c9b0
melissa kewagamang <mrbkew@gmail.com>
2015-08-04 12:46:21 +0200

    flake8

--- bhp065/apps/hnscc_subject/models/hnscc_off_study.py
+++ bhp065/apps/hnscc_subject/models/hnscc_off_study.py
@@ -2,13 +2,9 @@ from django.db import models
 from django.core.urlresolvers import reverse
 
 from edc.audit.audit_trail import AuditTrail
-# from edc.base.model.models import BaseUuidModel
-# from edc.subject.appointment_helper.models import BaseAppointmentMixin
-# from edc.subject.registration.models import RegisteredSubject
 from edc.subject.off_study.models.base_off_study import BaseOffStudy
 from edc.entry_meta_data.managers import EntryMetaDataManager
 
-# from .choices import OFF_STUDY_REASON
 from .hnscc_visit import HnsccVisit
 
 
