4afdb81a3d493fc0a8935444183a051ea294163a
melissa kewagamang <mrbkew@gmail.com>
2015-08-04 12:47:32 +0200

    meta method

--- bhp065/apps/hnscc_subject/models/hnscc_visit.py
+++ bhp065/apps/hnscc_subject/models/hnscc_visit.py
@@ -56,8 +56,8 @@ class HnsccVisit(HnsccOffStudyMixin, BaseVisitTracking):
 
     def save(self, *args, **kwargs):
         self.create_off_study_meta()
-        self.contemporary_meta()
-        self.historical_meta()
+        # self.contemporary_meta()
+        # self.historical_meta()
         super(HnsccVisit, self).save(*args, **kwargs)
 
     def create_off_study_meta(self):
@@ -79,77 +79,77 @@ class HnsccVisit(HnsccOffStudyMixin, BaseVisitTracking):
                 scheduled_meta_data.entry_status = 'NEW'
                 scheduled_meta_data.save()
 
-    def contemporary_meta(self):
-        if self.data_type == 'contemporary':
-            entry = Entry.objects.filter(model_name='historical',
-                                         visit_definition_id=self.appointment.visit_definition_id)
-            if entry:
-                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
-                    appointment=self.appointment,
-                    entry=entry[0],
-                    registered_subject=self.registered_subject)
-                if not scheduled_meta_data:
-                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
-                        appointment=self.appointment,
-                        entry=entry[0],
-                        registered_subject=self.registered_subject)
-                else:
-                    scheduled_meta_data = scheduled_meta_data[0]
-                scheduled_meta_data.entry_status = 'NOT REQUIRED'
-                scheduled_meta_data.save()
-        if not self.data_type == 'contemporary':
-            entry = Entry.objects.filter(model_name='historical',
-                                         visit_definition_id=self.appointment.visit_definition_id)
-            if entry:
-                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
-                    appointment=self.appointment,
-                    entry=entry[0],
-                    registered_subject=self.registered_subject)
-                if not scheduled_meta_data:
-                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
-                        appointment=self.appointment,
-                        entry=entry[0],
-                        registered_subject=self.registered_subject)
-                else:
-                    scheduled_meta_data = scheduled_meta_data[0]
-                scheduled_meta_data.entry_status = 'NEW'
-                scheduled_meta_data.save()
-
-    def historical_meta(self):
-        if self.data_type == 'historical':
-            entry = Entry.objects.filter(model_name='contemporary',
-                                         visit_definition_id=self.appointment.visit_definition_id)
-            if entry:
-                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
-                    appointment=self.appointment,
-                    entry=entry[0],
-                    registered_subject=self.registered_subject)
-                if not scheduled_meta_data:
-                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
-                        appointment=self.appointment,
-                        entry=entry[0],
-                        registered_subject=self.registered_subject)
-                else:
-                    scheduled_meta_data = scheduled_meta_data[0]
-                scheduled_meta_data.entry_status = 'NOT REQUIRED'
-                scheduled_meta_data.save()
-        if not self.data_type == 'historical':
-            entry = Entry.objects.filter(model_name='contemporary',
-                                         visit_definition_id=self.appointment.visit_definition_id)
-            if entry:
-                scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
-                    appointment=self.appointment,
-                    entry=entry[0],
-                    registered_subject=self.registered_subject)
-                if not scheduled_meta_data:
-                    scheduled_meta_data = ScheduledEntryMetaData.objects.create(
-                        appointment=self.appointment,
-                        entry=entry[0],
-                        registered_subject=self.registered_subject)
-                else:
-                    scheduled_meta_data = scheduled_meta_data[0]
-                scheduled_meta_data.entry_status = 'NEW'
-                scheduled_meta_data.save()
+#     def contemporary_meta(self):
+#         if self.data_type == 'contemporary':
+#             entry = Entry.objects.filter(model_name='historical',
+#                                          visit_definition_id=self.appointment.visit_definition_id)
+#             if entry:
+#                 scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+#                     appointment=self.appointment,
+#                     entry=entry[0],
+#                     registered_subject=self.registered_subject)
+#                 if not scheduled_meta_data:
+#                     scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+#                         appointment=self.appointment,
+#                         entry=entry[0],
+#                         registered_subject=self.registered_subject)
+#                 else:
+#                     scheduled_meta_data = scheduled_meta_data[0]
+#                 scheduled_meta_data.entry_status = 'NOT REQUIRED'
+#                 scheduled_meta_data.save()
+#         if not self.data_type == 'contemporary':
+#             entry = Entry.objects.filter(model_name='historical',
+#                                          visit_definition_id=self.appointment.visit_definition_id)
+#             if entry:
+#                 scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+#                     appointment=self.appointment,
+#                     entry=entry[0],
+#                     registered_subject=self.registered_subject)
+#                 if not scheduled_meta_data:
+#                     scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+#                         appointment=self.appointment,
+#                         entry=entry[0],
+#                         registered_subject=self.registered_subject)
+#                 else:
+#                     scheduled_meta_data = scheduled_meta_data[0]
+#                 scheduled_meta_data.entry_status = 'NEW'
+#                 scheduled_meta_data.save()
+#
+#     def historical_meta(self):
+#         if self.data_type == 'historical':
+#             entry = Entry.objects.filter(model_name='contemporary',
+#                                          visit_definition_id=self.appointment.visit_definition_id)
+#             if entry:
+#                 scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+#                     appointment=self.appointment,
+#                     entry=entry[0],
+#                     registered_subject=self.registered_subject)
+#                 if not scheduled_meta_data:
+#                     scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+#                         appointment=self.appointment,
+#                         entry=entry[0],
+#                         registered_subject=self.registered_subject)
+#                 else:
+#                     scheduled_meta_data = scheduled_meta_data[0]
+#                 scheduled_meta_data.entry_status = 'NOT REQUIRED'
+#                 scheduled_meta_data.save()
+#         if not self.data_type == 'historical':
+#             entry = Entry.objects.filter(model_name='contemporary',
+#                                          visit_definition_id=self.appointment.visit_definition_id)
+#             if entry:
+#                 scheduled_meta_data = ScheduledEntryMetaData.objects.filter(
+#                     appointment=self.appointment,
+#                     entry=entry[0],
+#                     registered_subject=self.registered_subject)
+#                 if not scheduled_meta_data:
+#                     scheduled_meta_data = ScheduledEntryMetaData.objects.create(
+#                         appointment=self.appointment,
+#                         entry=entry[0],
+#                         registered_subject=self.registered_subject)
+#                 else:
+#                     scheduled_meta_data = scheduled_meta_data[0]
+#                 scheduled_meta_data.entry_status = 'NEW'
+#                 scheduled_meta_data.save()
 
     class Meta:
         app_label = "hnscc_subject"
