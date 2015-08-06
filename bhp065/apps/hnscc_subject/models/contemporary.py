9588840dd2ff4784a94a654665d4c30c91418237
melissa kewagamang <mrbkew@gmail.com>
2015-08-06 16:07:29 +0200

    objects manager

--- bhp065/apps/hnscc_subject/models/contemporary.py
+++ bhp065/apps/hnscc_subject/models/contemporary.py
@@ -64,6 +64,8 @@ class Contemporary (BaseScheduledVisitModel):
     def get_absolute_url(self):
         return reverse('admin:hnscc_subject_contemporary_change', args=(self.id,))
 
+    objects = models.Manager()
+
     class Meta:
         app_label = "hnscc_subject"
         verbose_name = "Contemporary"
