4589a830983e720806a37c7fe4e5dd5ba4b0ad19
melissa kewagamang <mrbkew@gmail.com>
2015-12-18 08:28:36 +0200

    update search

--- bhp065/apps/hnscc_subject/search/hnscc_search_by_word.py
+++ bhp065/apps/hnscc_subject/search/hnscc_search_by_word.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from edc.dashboard.search.classes import BaseSearchByWord
 from ..models import Enrollment
 
@@ -8,3 +9,10 @@ class HnsccSearchByWord(BaseSearchByWord):
     search_model = Enrollment
     order_by = ['-created']
     template = 'enrollment_include.html'
+
+    @property
+    def qset(self):
+        qset = (
+            Q(registered_subject__subject_identifier__icontains=self.search_value) |
+            Q(pathology_no__icontains=self.search_value))
+        return qset
